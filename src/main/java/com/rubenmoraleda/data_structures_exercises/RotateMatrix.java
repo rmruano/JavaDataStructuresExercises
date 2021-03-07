b7afefb91727db4a06ba26c580720ffb79c37fb4
ruben <ruben.moraleda.ruano@gmail.com>
2021-03-07 19:44:33 +0100

    RotateMatrix Exercise (Rotate in place)

--- src/main/java/com/rubenmoraleda/data_structures_exercises/RotateMatrix.java
+++ src/main/java/com/rubenmoraleda/data_structures_exercises/RotateMatrix.java
@@ -15,16 +15,69 @@ public class RotateMatrix {
             matrix = new int[n][n];
         }
 
+        /**
+         * Swaps the axis and negates the Y
+         * @param x coordinate
+         * @param y coordinate
+         * @param w matrix width
+         * @param h matrix height
+         * @return
+         */
+        protected int[] getRotatedCoordinates(int x, int y, int w, int h) {
+            int[] rotatedCoords = new int[2];
+            rotatedCoords[0] = w-y-1;
+            rotatedCoords[1] = x;
+            return rotatedCoords;
+        }
+
+        /**
+         * Rotate the matrix easily by creating a new matrix.
+         * It iterates through the whole matrix to get the new value for each cell.
+         */
         public void rotateNotInPlace() {
             int w = matrix.length;
             int h = matrix[0].length;
             int ret[][] = new int[w][h];
+            // Swap the axis and negate the Y (iterate from the end)
             for(int i=0; i<w; i++) {
                 for (int j=0; j<h; j++) {
-                    ret[i][j] = matrix[w-j-1][i];
+                    int[] rotatedCoords = getRotatedCoordinates(i, j, w, h);
+                    ret[i][j] = matrix[rotatedCoords[0]][rotatedCoords[1]];
                 }
             }
             matrix = ret;
+        }
+
+        /**
+         * Rotate the matrix without creating a new one.
+         * We'll do that by rotating the 4 positions (360 deg) in place simultaneously.
+         * Therefore, the iterations must be performed only on top-left 1/4th of the matrix, the other 3/4ths will be
+         * rotated automatically. Please notice that the Y axis max value must be smaller than the X axis max.
+         */
+        public void rotateInPlace() {
+            int w = matrix.length;
+            int h = matrix[0].length;
+            int[] rotatedCoords1, rotatedCoords2, rotatedCoords3, values = new int[4];
+            // Swap the axis and negate the Y (iterate from the end)
+            for(int i=0, iMax=(int)Math.ceil((double)w/2D); i<iMax; i++) {
+                for (int j=0, jMax=(int)Math.floor((double)h/2D); j<jMax; j++) {
+                    // Get the 4 positions (360 deg) indices
+                    rotatedCoords1 = getRotatedCoordinates(i, j, w, h);
+                    rotatedCoords2 = getRotatedCoordinates(rotatedCoords1[0], rotatedCoords1[1], w, h);
+                    rotatedCoords3 = getRotatedCoordinates(rotatedCoords2[0], rotatedCoords2[1], w, h);
+                    values = new int[4];
+                    // Get the values of that positions
+                    values[0] = matrix[rotatedCoords1[0]][rotatedCoords1[1]];
+                    values[1] = matrix[rotatedCoords2[0]][rotatedCoords2[1]];
+                    values[2] = matrix[rotatedCoords3[0]][rotatedCoords3[1]];
+                    values[3] = matrix[i][j];
+                    // Store them rotated 90 degrees
+                    matrix[i][j] = values[0];
+                    matrix[rotatedCoords1[0]][rotatedCoords1[1]] = values[1];
+                    matrix[rotatedCoords2[0]][rotatedCoords2[1]] = values[2];
+                    matrix[rotatedCoords3[0]][rotatedCoords3[1]] = values[3];
+                }
+            }
         }
     }
 
--- src/test/java/com/rubenmoraleda/data_structures_exercises/RotateMatrixTest.java
+++ src/test/java/com/rubenmoraleda/data_structures_exercises/RotateMatrixTest.java
@@ -10,10 +10,22 @@ public class RotateMatrixTest extends TestCase {
         matrix.matrix[1] = new int[]{2, 3};
         matrix.rotateNotInPlace();
 
-        assertEquals(matrix.matrix[0][0], 2);
-        assertEquals(matrix.matrix[0][1], 0);
-        assertEquals(matrix.matrix[1][0], 3);
-        assertEquals(matrix.matrix[1][1], 1);
+        assertEquals(2, matrix.matrix[0][0]);
+        assertEquals(0, matrix.matrix[0][1]);
+        assertEquals(3, matrix.matrix[1][0]);
+        assertEquals(1, matrix.matrix[1][1]);
+    }
+
+    public void testRotateInPlace2x2Matrix() {
+        RotateMatrix.Matrix matrix = new RotateMatrix.Matrix(2);
+        matrix.matrix[0] = new int[]{0, 1};
+        matrix.matrix[1] = new int[]{2, 3};
+        matrix.rotateInPlace();
+
+        assertEquals(2, matrix.matrix[0][0]);
+        assertEquals(0, matrix.matrix[0][1]);
+        assertEquals(3, matrix.matrix[1][0]);
+        assertEquals(1, matrix.matrix[1][1]);
     }
 
     public void testRotate3x3Matrix() {
@@ -23,15 +35,85 @@ public class RotateMatrixTest extends TestCase {
         matrix.matrix[2] = new int[]{6, 7, 8};
         matrix.rotateNotInPlace();
 
-        assertEquals(matrix.matrix[0][0], 6);
-        assertEquals(matrix.matrix[0][1], 3);
-        assertEquals(matrix.matrix[0][2], 0);
-        assertEquals(matrix.matrix[1][0], 7);
-        assertEquals(matrix.matrix[1][1], 4);
-        assertEquals(matrix.matrix[1][2], 1);
-        assertEquals(matrix.matrix[2][0], 8);
-        assertEquals(matrix.matrix[2][1], 5);
-        assertEquals(matrix.matrix[2][2], 2);
+        assertEquals(6, matrix.matrix[0][0]);
+        assertEquals(3, matrix.matrix[0][1]);
+        assertEquals(0, matrix.matrix[0][2]);
+        assertEquals(7, matrix.matrix[1][0]);
+        assertEquals(4, matrix.matrix[1][1]);
+        assertEquals(1, matrix.matrix[1][2]);
+        assertEquals(8, matrix.matrix[2][0]);
+        assertEquals(5, matrix.matrix[2][1]);
+        assertEquals(2, matrix.matrix[2][2]);
+    }
+
+    public void testRotateInPlace3x3Matrix() {
+        RotateMatrix.Matrix matrix = new RotateMatrix.Matrix(3);
+        matrix.matrix[0] = new int[]{0, 1, 2};
+        matrix.matrix[1] = new int[]{3, 4, 5};
+        matrix.matrix[2] = new int[]{6, 7, 8};
+        matrix.rotateInPlace();
+
+        assertEquals(6, matrix.matrix[0][0]);
+        assertEquals(3, matrix.matrix[0][1]);
+        assertEquals(0, matrix.matrix[0][2]);
+        assertEquals(7, matrix.matrix[1][0]);
+        assertEquals(4, matrix.matrix[1][1]);
+        assertEquals(1, matrix.matrix[1][2]);
+        assertEquals(8, matrix.matrix[2][0]);
+        assertEquals(5, matrix.matrix[2][1]);
+        assertEquals(2, matrix.matrix[2][2]);
+    }
+
+    public void testRotate4x4Matrix() {
+        RotateMatrix.Matrix matrix = new RotateMatrix.Matrix(4);
+        matrix.matrix[0] = new int[]{0, 1, 2, 3};
+        matrix.matrix[1] = new int[]{4, 5, 6, 7};
+        matrix.matrix[2] = new int[]{8, 9, 0, 1};
+        matrix.matrix[3] = new int[]{2, 3, 4, 5};
+        matrix.rotateNotInPlace();
+
+        assertEquals(2, matrix.matrix[0][0]);
+        assertEquals(8, matrix.matrix[0][1]);
+        assertEquals(4, matrix.matrix[0][2]);
+        assertEquals(0, matrix.matrix[0][3]);
+        assertEquals(3, matrix.matrix[1][0]);
+        assertEquals(9, matrix.matrix[1][1]);
+        assertEquals(5, matrix.matrix[1][2]);
+        assertEquals(1, matrix.matrix[1][3]);
+        assertEquals(4, matrix.matrix[2][0]);
+        assertEquals(0, matrix.matrix[2][1]);
+        assertEquals(6, matrix.matrix[2][2]);
+        assertEquals(2, matrix.matrix[2][3]);
+        assertEquals(5, matrix.matrix[3][0]);
+        assertEquals(1, matrix.matrix[3][1]);
+        assertEquals(7, matrix.matrix[3][2]);
+        assertEquals(3, matrix.matrix[3][3]);
+    }
+
+    public void testRotateInPlace4x4Matrix() {
+        RotateMatrix.Matrix matrix = new RotateMatrix.Matrix(4);
+        matrix.matrix[0] = new int[]{0, 1, 2, 3};
+        matrix.matrix[1] = new int[]{4, 5, 6, 7};
+        matrix.matrix[2] = new int[]{8, 9, 0, 1};
+        matrix.matrix[3] = new int[]{2, 3, 4, 5};
+        matrix.rotateInPlace();
+
+        assertEquals(2, matrix.matrix[0][0]);
+        assertEquals(8, matrix.matrix[0][1]);
+        assertEquals(4, matrix.matrix[0][2]);
+        assertEquals(0, matrix.matrix[0][3]);
+        assertEquals(3, matrix.matrix[1][0]);
+        assertEquals(9, matrix.matrix[1][1]);
+        assertEquals(5, matrix.matrix[1][2]);
+        assertEquals(1, matrix.matrix[1][3]);
+        assertEquals(4, matrix.matrix[2][0]);
+        assertEquals(0, matrix.matrix[2][1]);
+        assertEquals(6, matrix.matrix[2][2]);
+        assertEquals(2, matrix.matrix[2][3]);
+        assertEquals(5, matrix.matrix[3][0]);
+        assertEquals(1, matrix.matrix[3][1]);
+        assertEquals(7, matrix.matrix[3][2]);
+        assertEquals(3, matrix.matrix[3][3]);
     }
 
 }
